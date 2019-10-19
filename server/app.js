3a1b3ef3aff901bc9fe4b78f9108716bc8c88a6b
Harry Chen <harrychenhc@icloud.com>
2019-10-19 03:14:22 -0400

    added a fetch all route to help you guys develop frontend while more complex backend actions get built

--- server/app.js
+++ server/app.js
@@ -5,8 +5,15 @@ require('./db/connection')(app) // connecting db
 // body parsing middleware for json
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res, next) => {
-  res.json({ msg: 'Hello World' })
+// api middleware
+app.use('/api', require('./routes'))
+
+
+// final error catcher just incase an error didn't get handled
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  const msg = err.message || 'Server Error'
+  res.status(status).json({ msg })
 })
 
 module.exports = app
