16a50e5ab5b185cad6533ee8ef174881f13d0470
Harry C <harrychenhc@icloud.com>
2019-10-19 15:30:40 -0400

    added validation to query params and body

--- server/routes/foodRouter.js
+++ server/routes/foodRouter.js
@@ -1,5 +1,5 @@
 const Router = require('express').Router()
-const { body, validationResult } = require('express-validator')
+const { body, query, validationResult } = require('express-validator')
 
 const Food = require('../db/models/Food')
 
@@ -15,16 +15,74 @@ Router.get('/', (req, res, next) => {
 })
 
 /*
+  @route          GET api/foods/search?longitude=40.23232&latitude=74.521322
+  @description    fetches all food within a specified distance from you
+  @access         public
+*/
+Router.get('/search?', [
+  query('latitude', 'Latitude is required').exists().isString().isLength({ min: 4 }),
+  query('longitude', 'Longitude is required').exists().isString().isLength({ min: 4 }),
+  query('range', 'Range is required').exists().isString().isLength({ min: 1 })
+], async (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+
+  let { latitude, longitude, range, food } = req.query
+
+  if (food) {
+    return next() // if food query exists, pass the request to the next middleware
+  }
+
+  // converting to type numb
+  range *= 1
+  longitude *= 1
+  latitude *= 1
+
+  try {
+    const foodData = await Food.find({
+      longitude: {
+        $gte: longitude - range,
+        $lt: longitude + range
+      },
+      latitude: {
+        $gte: latitude - range,
+        $lt: latitude + range
+      }
+    })
+
+    if (foodData.length) {
+      res.status(200).json(foodData)
+    } else {
+      res.status(200).json({
+        msg: 'No food found within that range'
+      })
+    }
+
+  } catch (err) {
+    next(err)
+  }
+})
+
+/*
   @route          GET api/foods/search?longitude=40.23232&latitude=74.521322&food=strawberries&range=4.2
   @description    fetches a specific foods within a specified distance from you
   @access         public
 */
-Router.get('/search?', async (req, res, next) => {
+Router.get('/search?', [
+  query('food', 'Food must require a name').exists().isString(),
+  query('latitude', 'Latitude is required').exists().isString().isLength({ min: 4 }),
+  query('longitude', 'Longitude is required').exists().isString().isLength({ min: 4 }),
+  query('range', 'Range is required').exists().isString().isLength({ min: 1 })
+], async (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+
   let { longitude, latitude, food, range } = req.query
 
-  if (!food) { // if  there is no food query then we go to the next search
-    return next()
-  }
   // converting to type numb
   range *= 1
   longitude *= 1
@@ -57,53 +115,15 @@ Router.get('/search?', async (req, res, next) => {
 })
 
 /*
-  @route          GET api/foods/search?longitude=40.23232&latitude=74.521322
-  @description    fetches all food within a specified distance from you
-  @access         public
-*/
-Router.get('/search?', async (req, res, next) => {
-  let { latitude, longitude, range } = req.query
-
-  // converting to type numb
-  range *= 1
-  longitude *= 1
-  latitude *= 1
-
-  try {
-    const foodData = await Food.find({
-      longitude: {
-        $gte: longitude - range,
-        $lt: longitude + range
-      },
-      latitude: {
-        $gte: latitude - range,
-        $lt: latitude + range
-      }
-    })
-
-    if (foodData.length) {
-      res.status(200).json(foodData)
-    } else {
-      res.status(200).json({
-        msg: 'No food found within that range'
-      })
-    }
-
-  } catch (err) {
-    next(err)
-  }
-})
-
-/*
   @route          POST api/foods/
   @description    user creates a new food instance in our database
   @access         public
 */
 Router.post('/', [
-  body('name').exists().isString(),
-  body('price').exists().isString(),
-  body('longitude').exists().isString().isLength({ min: 2 }),
-  body('latitude').exists().isString().isLength({ min: 2 })
+  body('name', 'Food must require a name').exists().isString(),
+  body('price', 'Price is required').exists().isString(),
+  body('longitude', 'Longitude is required').exists().isString().isLength({ min: 4 }),
+  body('latitude', 'Latitude is required').exists().isString().isLength({ min: 4 })
 ], async (req, res, next) => {
 
   const errors = validationResult(req)
