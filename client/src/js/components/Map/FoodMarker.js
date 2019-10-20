4ca4062c6eb8cfb73ebcd0acc5b5f48d3eb261b5
Harry C <harrychenhc@icloud.com>
2019-10-20 02:51:47 -0400

    updated markers and seed gps coord doesnt seem to be in water

--- client/src/js/components/Map/FoodMarker.js
+++ client/src/js/components/Map/FoodMarker.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
 
 const FoodMarkerStyles = {
-
+  position: 'absolute',
+  width: 40,
+  height: 40,
+  left: -40 / 2,
+  right: -40 / 2,
+  border: '5px solid #f44336',
+  borderRadius: 40,
+  backgroundColor: 'white',
+  textAlign: 'center',
+  color: '#3f51b5',
+  fontSize: 16,
+  fontWeight: 'bold',
+  padding: 4
 }
 
 class FoodMarker extends Component {
   render() {
+    const { key } = this.props
     return (
       <div
-        style={{
-          height: '40px',
-          width: '40px',
-          zIndex: 2,
-          backgroundColor: 'red'
-        }}
+        key={key}
+        style={FoodMarkerStyles}
       >
       </div>
     )
